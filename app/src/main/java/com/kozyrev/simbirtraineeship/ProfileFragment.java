4158e4d03615543f09304a29427162dc8fe3439d
Kandry <andry6522227@yandex.ru>
2019-11-21 12:22:14 +0400

    Профиль. Фрагмент и выравнивание сверху/снизу

---
+++ app/src/main/java/com/kozyrev/simbirtraineeship/ProfileFragment.java
@@ -0,0 +1,25 @@
+package com.kozyrev.simbirtraineeship;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class ProfileFragment extends Fragment {
+
+    public ProfileFragment() {}
+
+    public static ProfileFragment newInstance(String param1, String param2) {
+        ProfileFragment fragment = new ProfileFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        return view;
+    }
+}
